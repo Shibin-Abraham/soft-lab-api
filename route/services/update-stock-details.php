c0320b14be51ea0853b2747b4f99b740fe995aee
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-04-13 20:12:00 +0530

    update-stock service created

---
+++ route/services/update-stock-details.php
@@ -0,0 +1,54 @@
+<?php include("../../config/constants.php");
+    include('../middleware/jwt-auth.php');
+    header('Content-type:JSON');
+    $request = file_get_contents("php://input",true);
+    $data = json_decode($request);
+    $allheaders = getallheaders();
+    if(!empty($allheaders['Authorization'])){
+        $auth_result = JWTStatus($allheaders['Authorization']);
+        if($auth_result['statuscode'] === 200 && $auth_result['status'] === '1'){
+            if(isset($data->stockid,$data->stockcategory,$data->sname,$data->invoiceid,$data->current_date,$data->date,$data->current_time,$data->stocktype,$data->stock_name)){
+                $sql = "UPDATE `stock` SET `type` = '$data->stocktype', `category` = '$data->stockcategory', `invoice_id` = '$data->invoiceid', `invoice_date` = '$data->date', `supplier_name` = '$data->sname' WHERE `id` = $data->stockid;";
+                $result = mysqli_query($conn,$sql);
+                if($result){
+                    $name = $auth_result['name'];
+                    $email = $auth_result['email'];
+                    $sql = "INSERT INTO `recent_activities` (`id`, `name`, `email`, `date`, `time`, `details`, `operation`) VALUES (NULL, '$name', '$email', '$data->current_date', '$data->current_time', '$data->stock_name', 'updated');";
+                    $result = mysqli_query($conn,$sql);
+                    if($result){
+                        $response = array(
+                            "statuscode" => 200 // success
+                        );
+                        echo json_encode($response,JSON_PRETTY_PRINT);
+                    }else{
+                        $response = array(
+                            "statuscode" => 500 // internal server error
+                        );
+                        echo json_encode($response,JSON_PRETTY_PRINT);
+                    }
+                }else{
+                    $response = array(
+                        "statuscode" => 500 // internal server error
+                    );
+                    echo json_encode($response,JSON_PRETTY_PRINT);
+                }
+            }else{
+                $response = array(
+                    "statuscode" => 400 // 400 bad request
+                );
+                echo json_encode($response,JSON_PRETTY_PRINT);
+            }
+        }else{
+            $response = array(
+                "statuscode" => 401 // 401 token expired
+            );
+            echo json_encode($response,JSON_PRETTY_PRINT); //token expired then login again
+        }
+    }else{
+        $response = array(
+            "statuscode" => 400 // 400 bad request
+        );
+        echo json_encode($response,JSON_PRETTY_PRINT);
+    }
+    
+?>
