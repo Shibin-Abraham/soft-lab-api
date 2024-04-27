626abe4142134459f101cb01494a94ed37b9b318
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-04-27 16:49:39 +0530

    borrowers update done

---
+++ route/services/update-borrower-data.php
@@ -0,0 +1,54 @@
+<?php include("../../config/constants.php");
+    include('../middleware/jwt-auth.php');
+    header('Content-type:JSON');
+    $request = file_get_contents("php://input",true);
+    $data = json_decode($request);
+    $allheaders = getallheaders();
+    if(!empty($allheaders['Authorization'])){
+        $auth_result = JWTStatus($allheaders['Authorization']);
+        if($auth_result['statuscode'] === 200 && $auth_result['status'] === '1'){
+            if(isset($data->itemno,$data->bId,$data->date,$data->time)){
+                $sql = "UPDATE `borrowers` SET `return_status` = '1' WHERE `id` = $data->bId;";
+                $result = mysqli_query($conn,$sql);
+                if($result){
+                    $name = $auth_result['name'];
+                    $email = $auth_result['email'];
+                    $sql = "INSERT INTO `recent_activities` (`id`, `name`, `email`, `date`, `time`, `details`, `operation`) VALUES (NULL, '$name', '$email', '$data->date', '$data->time', '$data->itemno', 'returned');";
+                    $result = mysqli_query($conn,$sql);
+                    if($result){
+                        $response = array(
+                            "statuscode" => 200 // success
+                        );
+                        echo json_encode($response,JSON_PRETTY_PRINT);
+                    }else{
+                        $response = array(
+                            "statuscode" => 500 // internal server error
+                        );
+                        echo json_encode($response,JSON_PRETTY_PRINT);
+                    }
+                }else{
+                    $response = array(
+                        "statuscode" => 500 // internal server error
+                    );
+                    echo json_encode($response,JSON_PRETTY_PRINT);
+                }
+            }else{
+                $response = array(
+                    "statuscode" => 400 // 400 bad request
+                );
+                echo json_encode($response,JSON_PRETTY_PRINT);
+            }
+        }else{
+            $response = array(
+                "statuscode" => 401 // 401 token expired
+            );
+            echo json_encode($response,JSON_PRETTY_PRINT); //token expired then login again
+        }
+    }else{
+        $response = array(
+            "statuscode" => 400 // 400 bad request
+        );
+        echo json_encode($response,JSON_PRETTY_PRINT);
+    }
+    
+?>
